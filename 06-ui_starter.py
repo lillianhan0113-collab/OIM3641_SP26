caccfcf348840020be4eac6bda02ade9620279e0
“Lillian <lillianhan0113@gmail.com>
2026-02-09 18:21:44 -0500

    0209 class

--- 06-ui_starter.py
+++ 06-ui_starter.py
@@ -4,7 +4,7 @@ import plotly.express as px
 import streamlit as st
 import yfinance as yf
 
-st.set_page_config(layout="wide", page_title="Stock Analyzer")
+st.set_page_config(layout='wide', page_title="Stock Analyzer")
 
 # CONSTANTS
 END = date.today()
@@ -22,14 +22,13 @@ def get_stock_data(ticker, start, end):
     except Exception as e:
         return None, f"Error {e}"
 
-
 # sidebar
-st.sidebar.title("input")
+st.sidebar.title("🗠 Inputs")
 ticker = st.sidebar.text_input("Stock symbol", value="AAPL").upper()
 col1, col2 = st.sidebar.columns(2)
-start = col1.date_input("Start date", value=START)
+start = col1.date_input("Start Date", value=START)
 end = col2.date_input("End Date", value=END)
-moving_average = st.sidebar.sliden("Moving Average Window",
+moving_average = st.sidebar.slider("Moving Average Window",
                                    min_value=10,
                                    max_value=100,
                                    value=50,
@@ -38,6 +37,7 @@ run = st.sidebar.button("Run Analysis", type='primary')
 
 #main UI
 st.title("Stock Analyzer")
+
 if run:
     with st.spinner(f"Fetching {ticker} data..."):
         df, message = get_stock_data(ticker, start, end)
@@ -45,24 +45,23 @@ if run:
         st.sidebar.success(message)
     else:
         st.sidebar.error(message)
-    df['NA'] = df['Close'].rolling(window=moving_average).mean()
-    df['pot_change'] = df['Close'].pct_change() *100
+    df['MA'] = df['Close'].rolling(window=moving_average).mean()
+    df['pct_change'] = df['Close'].pct_change() * 100
 
-# output
-tab1, tab2, tab3 = st.tabs(['chart','Statistics','Raw Data'])
-with (tab1):
-    st.subheader(f"{ticker} High Level Analysis")
-    col1, col2, col3 = st.columns(3)
-    col1.metric(f'Latest Price', f'{df['Close'].iloc[-1]:.2f}')
-    col2.metric(f'Cum Change',
-                f'{df['Close'].iloc[-1]/df['Close'].iloc[0] -1):.2f}')
-    col3.metric(f'Trading Days', f'{len(df)}')
-    figure = px.line(df, y= ["Close", "MA"])
-    figure.update_layout(hover) #没写完
-    with st.spinner("Creating Chart"):
-        st.plotly_chart(figure, use_container_wihin = True)
+    # output
+    tab1, tab2, tab3 = st.tabs(['📉 Chart', ' 📊Statistics', ' 🗄️Raw Data'])
+    with tab1:
+        st.subheader(f"{ticker} High Level Analysis")
+        col1, col2, col3 = st.columns(3)
+        col1.metric(label='Latest Price', value=f"{df['Close'].iloc[-1]:.2f}")
+        col2.metric(label='Cum Change', value=f"{((df['Close'].iloc[-1] / df['Close'].iloc[0]) - 1) * 100:.2f}%")
+        col3.metric(label='Trading Days', value=f"{len(df)}")
+        figure = px.line(df, y= ['Close', 'MA'])
+        figure.update_layout(hovermode='x unified')
+        with st.spinner("Creating chart"):
+            st.plotly_chart(figure, use_container_width=True)
 
-    with (tab2):
+    with tab2:
         st.subheader("Summary Statistics")
         col1, col2 = st.columns(2)
         with col1:
@@ -71,19 +70,25 @@ with (tab1):
             st.dataframe(summary)
         with col2:
             price_stats = pd.DataFrame(
-                {'Metric':['High', 'Low', 'Mean', 'Volatility'],
-                 'Value': [
+                {'Metric': ['High', 'Low', 'Mean', 'Volatility'],
+                 'Values': [
                      f"{df["Close"].max()}",
-                     f"{df["Close"].min()}"
-                     f"{df["Close"].mean()}"
+                     f"{df["Close"].min()}",
+                     f"{df["Close"].mean()}",
                      f"{df["Close"].std()}"
                  ]
-
-
                 }
             )
-            st.dataframe()
+            st.dataframe(price_stats)
 
-
-
+    with tab3:
+        st.subheader("Raw Data")
+        st.dataframe(df.tail(10))
+        csv = df.to_csv()
+        st.download_button(
+                           label="Download Data",
+                           data = csv,
+                           file_name = f"{ticker}_{start}_{end}.csv",
+                           mime="text/csv"
+                            )
 
